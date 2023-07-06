806762c298727eb7f2ff9a5aba1ed1b199e88b59
Michal Gawlas <gawlas05@gmail.com>
2023-07-06 21:16:29 +0200

    small visual changes

--- src/main/java/com/library/dbService/mapper/BookMapper.java
+++ src/main/java/com/library/dbService/mapper/BookMapper.java
@@ -11,21 +11,22 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class BookMapper {
 
     private final TitleDbService titleDbService;
 
-    public BookDto mapToBookDto (final Book book) {
-        return new BookDto (
+    public BookDto mapToBookDto(final Book book) {
+        return new BookDto(
                 book.getBookId(),
                 book.getStatus(),
                 book.getTitle().getTitleId()
         );
     }
 
-    public Book mapToBook (final BookDto bookDto) throws BookNotFoundException {
+    public Book mapToBook(final BookDto bookDto) throws BookNotFoundException {
         return new Book(
                 bookDto.getBookId(),
                 bookDto.getStatus(),
@@ -33,7 +34,7 @@ public class BookMapper {
         );
     }
 
-    public List<BookDto> mapToBookDtoList (final List<Book> books){
+    public List<BookDto> mapToBookDtoList(final List<Book> books) {
         return books.stream()
                 .map(this::mapToBookDto)
                 .collect(Collectors.toList());
